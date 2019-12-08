bed840b2ebd62781d40aba7ac2b808a140b2eaea
maoyuanming <mym0806@163.com>
2019-12-08 23:54:47 +0800

    [flink]wc流式处理调整

--- Flink_Learn/src/main/scala/com/mym/wc/StreamWordCount.scala
+++ Flink_Learn/src/main/scala/com/mym/wc/StreamWordCount.scala
@@ -6,14 +6,19 @@ import org.apache.flink.streaming.api.scala._
 object StreamWordCount {
 
   def main(args: Array[String]): Unit = {
-//    val params = ParameterTool.fromArgs(args);
-//    val host:String = params.get("host");
-//    val port:Int = params.getInt("port")
+    val params = ParameterTool.fromArgs(args);
+    val host: String = params.get("host");
+    val port: Int = params.getInt("port")
 
     // 创建执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val testDataStream = env.socketTextStream("192.168.31.201", 7777)
+    var testDataStream : DataStream[scala.Predef.String] = null
+    if (host.nonEmpty) {
+      testDataStream = env.socketTextStream(host, port)
+    } else {
+      testDataStream = env.socketTextStream("192.168.31.201", 7777)
+    }
 
     // 逐一读取数据，分词后进行wc
     val wordCountDataStream = testDataStream.flatMap(_.split("\\s"))
