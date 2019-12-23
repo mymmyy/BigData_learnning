606dc7eecf538c6bb306506b68b1e7656085b194
maoyuanming <mym0806@163.com>
2019-12-23 15:04:58 +0800

    [flink]app渠道统计

---
+++ Spark_CookBook/src/main/scala/com/mym/spark/practice/source/SourceTest.scala
@@ -0,0 +1,12 @@
+package com.mym.spark.practice.source;
+
+object SourceTest {
+
+  def main(args: Array[String]): Unit = {
+    val localPath = "D:\\GitTest\\BigData_learnning\\Spark_CookBook\\src\\main\\resources\\source"
+
+
+
+  }
+
+}
