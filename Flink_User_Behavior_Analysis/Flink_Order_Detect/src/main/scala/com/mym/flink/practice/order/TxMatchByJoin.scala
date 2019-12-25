92688f7552d25a2f87735557d1c7d95b69193e8c
maoyuanming <mym0806@163.com>
2019-12-26 01:25:21 +0800

    [flink]join

---
+++ Flink_User_Behavior_Analysis/Flink_Order_Detect/src/main/scala/com/mym/flink/practice/order/TxMatchByJoin.scala
@@ -0,0 +1,59 @@
+package com.mym.flink.practice.order
+
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.util.Collector
+
+
+
+object TxMatchByJoin {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    // 读取订单事件流
+    val resource = getClass.getResource("/OrderLog.csv")
+        val orderEventStream = env.readTextFile(resource.getPath)
+//    val orderEventStream = env.socketTextStream("localhost", 7777)
+      .map(data => {
+        val dataArray = data.split(",")
+        OrderEvent(dataArray(0).trim.toLong, dataArray(1).trim, dataArray(2).trim, dataArray(3).trim.toLong)
+      })
+      .filter(_.txId != "")
+      .assignAscendingTimestamps(_.eventTime * 1000L)
+      .keyBy(_.txId)
+
+    // 读取支付到账事件流
+    val receiptResource = getClass.getResource("/ReceiptLog.csv")
+        val receiptEventStream = env.readTextFile(receiptResource.getPath)
+//    val receiptEventStream = env.socketTextStream("localhost", 8888)
+      .map( data => {
+        val dataArray = data.split(",")
+        ReceiptEvent( dataArray(0).trim, dataArray(1).trim, dataArray(2).toLong )
+      } )
+      .assignAscendingTimestamps(_.eventTime * 1000L)
+      .keyBy(_.txId)
+
+
+    //join处理
+    val processedJoinStream = orderEventStream.intervalJoin(receiptEventStream)
+      .between(Time.seconds(-5), Time.seconds(5))
+      .process(new TxPayMatchJoin())
+
+    processedJoinStream.print()
+
+    env.execute("tx pay match by join job")
+  }
+
+  class TxPayMatchJoin() extends ProcessJoinFunction[OrderEvent, ReceiptEvent, (OrderEvent, ReceiptEvent)]{
+    override def processElement(left: OrderEvent, right: ReceiptEvent, ctx: ProcessJoinFunction[OrderEvent, ReceiptEvent, (OrderEvent, ReceiptEvent)]#Context, out: Collector[(OrderEvent, ReceiptEvent)]): Unit = {
+      // 都是正常匹配上的数据
+      out.collect((left, right))
+    }
+  }
+
+}
